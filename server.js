cb4f0041f36ad19a65e4b056580aa5442ab2e09c
Eric Cabrera <Ecabrera@Erics-MacBook-Pro.local>
2019-09-09 17:14:10 -0500

    MVP

--- server.js
+++ server.js
@@ -6,4 +6,45 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/accounts', (req, res) => {
+    db('Accounts')
+        .then(accounts => res.status(200).json(accounts))
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
+});
+
+server.post('/accounts', (req, res) => {
+    db('Accounts')
+        .insert(req.body)
+        .then(([totalUsers]) => res.status(201).json(totalUsers))
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
+});
+
+server.put('/accounts/:id', (req, res) => {
+    const { id } = req.params;
+    db('Accounts')
+        .where({ id: id})
+        .update(req.body)
+        .then(updated => res.status(201).json(updated))
+})
+
+server.delete('/accounts/:id', (req, res) => {
+    const { id } = req.params;
+    db('accounts')
+        .where({ id: id}).del()
+        .then(resp => {
+            res.status(200);
+            console.log(resp);
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
+});
+
 module.exports = server;
