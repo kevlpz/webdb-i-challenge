d2edd29629fdd724d4c78594a662265eddce32fa
Eric Cabrera <Ecabrera@Erics-MacBook-Pro.local>
2019-09-09 18:51:27 -0500

    gets specific user

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ server.get('/accounts', (req, res) => {
         });
 });
 
+server.get('/accounts/:id', (req, res) => {
+    const { id } = req.params;
+    db('Accounts')
+        .where({id: id})
+        .then(account => res.status(200).json(account))
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
+});
+
 server.post('/accounts', (req, res) => {
     db('Accounts')
         .insert(req.body)
@@ -36,10 +47,10 @@ server.put('/accounts/:id', (req, res) => {
 server.delete('/accounts/:id', (req, res) => {
     const { id } = req.params;
     db('accounts')
-        .where({ id: id}).del()
-        .then(resp => {
-            res.status(200);
-            console.log(resp);
+        .where({ id: id }).del()
+        .then(deleted => {
+            res.status(200).json(deleted);
+            console.log(deleted);
         })
         .catch(err => {
             console.log(err);
